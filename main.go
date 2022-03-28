f575d8141b6538e9acf8fc488ecce622de2d50cd
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-03-28 22:48:02 +0200

    add go echo

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main (){
+import (
+	"github.com/hmada15/go-food-order/routes"
+	"github.com/labstack/echo/v4"
+)
 
+func main() {
+	e := echo.New()
+	
+	routes.Routes(e)
+	
+	e.Logger.Fatal(e.Start(":1323"))
 }
