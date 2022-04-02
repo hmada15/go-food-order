00f5947037bf89b85073ef71df42a9de54fd2e80
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-04-02 14:16:19 +0200

    add StrToInt32 and StrToBool helper function

--- helper/helper.go
+++ helper/helper.go
@@ -2,8 +2,26 @@ package helper
 
 import "fmt"
 
-func StrToInt(str string) int32 {
+func StrToInt32(str string) int32 {
 	var integer int32 = 0
 	fmt.Sscan(str, &integer)
 	return integer
+}
+
+func StrToInt(str string) int {
+	var integer int = 0
+	fmt.Sscan(str, &integer)
+	return integer
+}
+
+func StrToBool(str string) bool {
+	var boolean bool
+
+	if str == "true" || str == "TRUE" || str == "True" {
+		boolean = true
+	} else if str == "false" || str == "FALSE" || str == "False" {
+		boolean = false
+	}
+
+	return boolean
 }
